609dbf3abe033b6f282e2e00b226cb7332be0867
Nguyễn Mạnh Thắng <nguyenmanhthang2000.fb@gmail.com>
2021-05-19 08:50:01 +0700

    responsive new cv

--- js/main.js
+++ js/main.js
@@ -25,4 +25,27 @@ $(function () {
             $("#scroll-on-top").css("display", "none");
         }
     })
+
+    // RES
+    $(".main-button").click(function() {
+        $(".main-sidebar").css("left", "0");
+        $(".main-sidebar").css("z-index", "2");
+        $(".main-sidebar").css("background-color", "white");
+        $(".main-modal").css("display", "block");
+    })
+
+    $(".main-sidebar__wrapper").click(function() {
+        $(".main-modal").css("display", "none");
+        $(".main-sidebar").css("left", "-200%");
+    })
+
+    $(".main-modal").click(function() {
+        $(".main-modal").css("display", "none");
+        $(".main-sidebar").css("left", "-200%");
+    })
+
+    $("#close-sidebar").click(function() {
+        $(".main-modal").css("display", "none");
+        $(".main-sidebar").css("left", "-200%");
+    })
 })
