f4be91da6f46ca1be635986b474f74115df2d4dc
ngmthaq <nguyenmanhthang2000.fb@gmail.com>
2023-02-13 20:20:04 +0700

    Upgrade CV

--- js/main.js
+++ js/main.js
@@ -1,51 +1,220 @@
 $(function () {
-    $(".main-sidebar__wrapper").click(function () {
-        $(".active").eq(0).removeClass("active");
-        $(this).addClass(" active");
-    })
+  const rgba = "rgba(25, 28, 31, 0.8)";
 
-    $("#scroll-on-top").click(function () {
-        $(".active").eq(0).removeClass("active");
-        $("a[href='#home']").addClass(" active");
-    })
+  getHeader().then(
+    getFooter().then(
+      getHome().then(
+        getAbout().then(
+          getResume().then(
+            getSkill().then(
+              getPortfolio().then(
+                getContact().then(function () {
+                  init();
+                })
+              )
+            )
+          )
+        )
+      )
+    )
+  );
 
-    $(".main-content-item").mousemove(function () {
-        let id = $(this).attr("id");
-        if (id == "skills") {
-            id = "about";
-        }
-        $(".active").eq(0).removeClass("active");
-        $("a[href='#" + id + "']").addClass(" active");
-    })
+  function init() {
+    let hash = window.location.hash;
+    if (!hash) hash = "#home";
+    hash = hash.replace("#", "");
 
-    $(window).scroll(function () {
-        if ($(window).scrollTop() > 40) {
-            $("#scroll-on-top").css("display", "block");
-        } else {
-            $("#scroll-on-top").css("display", "none");
-        }
-    })
+    let top = $(document).scrollTop();
+    $("header").css("height", top > 120 ? "60px" : "80px");
+    $("header").css("background", top > 120 ? rgba : "transparent");
 
-    // RES
-    $(".main-button").click(function() {
-        $(".main-sidebar").css("left", "0");
-        $(".main-sidebar").css("z-index", "2");
-        $(".main-sidebar").css("background-color", "white");
-        $(".main-modal").css("display", "block");
-    })
+    $(document).find(`.custom_navbar a#header_${hash}`).addClass("active");
 
-    $(".main-sidebar__wrapper").click(function() {
-        $(".main-modal").css("display", "none");
-        $(".main-sidebar").css("left", "-200%");
-    })
+    $(document).scroll(onDocumentScroll);
+    $(document).on("mouseover", ".body_section", onMouseOver);
+    $(window).on("hashchange", onHashChange);
 
-    $(".main-modal").click(function() {
-        $(".main-modal").css("display", "none");
-        $(".main-sidebar").css("left", "-200%");
-    })
+    $(document).find("#skill_slider").slick({
+      dots: false,
+      infinite: true,
+      arrows: false,
+      swipeToSlide: true,
+      speed: 300,
+      slidesToShow: 6,
+      autoplay: true,
+      autoplaySpeed: 3000,
+    });
+  }
 
-    $("#close-sidebar").click(function() {
-        $(".main-modal").css("display", "none");
-        $(".main-sidebar").css("left", "-200%");
-    })
-})
+  function onDocumentScroll() {
+    let top = $(this).scrollTop();
+    $("header").css("height", top > 120 ? "60px" : "80px");
+    $("header").css("background", top > 120 ? rgba : "transparent");
+  }
+
+  function onHashChange() {
+    let hash = window.location.hash;
+    if (!hash) hash = "#home";
+    hash = hash.replace("#", "");
+    $(document).find(`.custom_navbar a`).removeClass("active");
+    $(document).find(`.custom_navbar a#header_${hash}`).addClass("active");
+  }
+
+  function onMouseOver(e) {
+    e.stopPropagation();
+    let element = e.currentTarget;
+    let id = element.id;
+
+    if (history.pushState) {
+      history.pushState(null, null, "#" + id);
+    } else {
+      location.hash = "#" + id;
+    }
+
+    onHashChange();
+  }
+
+  function getFooter() {
+    return new Promise((res, rej) => {
+      try {
+        $.ajax({
+          type: "GET",
+          url: "/html/footer.html",
+          dataType: "html",
+          success: function (response) {
+            $("footer").html(response);
+            res(true);
+          },
+        });
+      } catch (error) {
+        rej(error);
+      }
+    });
+  }
+
+  function getHeader() {
+    return new Promise((res, rej) => {
+      try {
+        $.ajax({
+          type: "GET",
+          url: "/html/header.html",
+          dataType: "html",
+          success: function (response) {
+            $("header").html(response);
+            res(true);
+          },
+        });
+      } catch (error) {
+        rej(error);
+      }
+    });
+  }
+
+  function getAbout() {
+    return new Promise((res, rej) => {
+      try {
+        $.ajax({
+          type: "GET",
+          url: "/html/about.html",
+          dataType: "html",
+          success: function (response) {
+            $("#about").html(response);
+            res(true);
+          },
+        });
+      } catch (error) {
+        rej(error);
+      }
+    });
+  }
+
+  function getContact() {
+    return new Promise((res, rej) => {
+      try {
+        $.ajax({
+          type: "GET",
+          url: "/html/contact.html",
+          dataType: "html",
+          success: function (response) {
+            $("#contact").html(response);
+            res(true);
+          },
+        });
+      } catch (error) {
+        rej(error);
+      }
+    });
+  }
+
+  function getHome() {
+    return new Promise((res, rej) => {
+      try {
+        $.ajax({
+          type: "GET",
+          url: "/html/home.html",
+          dataType: "html",
+          success: function (response) {
+            $("#home").html(response);
+            res(true);
+          },
+        });
+      } catch (error) {
+        rej(error);
+      }
+    });
+  }
+
+  function getPortfolio() {
+    return new Promise((res, rej) => {
+      try {
+        $.ajax({
+          type: "GET",
+          url: "/html/portfolio.html",
+          dataType: "html",
+          success: function (response) {
+            $("#portfolio").html(response);
+            res(true);
+          },
+        });
+      } catch (error) {
+        rej(error);
+      }
+    });
+  }
+
+  function getResume() {
+    return new Promise((res, rej) => {
+      try {
+        $.ajax({
+          type: "GET",
+          url: "/html/resume.html",
+          dataType: "html",
+          success: function (response) {
+            $("#resume").html(response);
+            res(true);
+          },
+        });
+      } catch (error) {
+        rej(error);
+      }
+    });
+  }
+
+  function getSkill() {
+    return new Promise((res, rej) => {
+      try {
+        $.ajax({
+          type: "GET",
+          url: "/html/skills.html",
+          dataType: "html",
+          success: function (response) {
+            $("#skills").html(response);
+            res(true);
+          },
+        });
+      } catch (error) {
+        rej(error);
+      }
+    });
+  }
+});
